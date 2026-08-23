c386f86c20c102964870daeedc97c0ca33665e34
ElhaeD <diegoi.98.cum@gmail.com>
2026-08-23 13:58:41 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import agents from "agents/vite";
 
 export default defineConfig({
-  plugins: [cloudflare()]
+  plugins: [agents(), cloudflare()]
 });
